e12a31a878dd6bd26a9e33e984d7a80b9952c86f
merceralex397-collab <merceralex397@gmail.com>
2026-09-07 07:26:18 +0100

    Reject retired EVA work in dispatcher and health projections

--- src/Pegasus.Core/Operations/ServiceHealth.cs
+++ src/Pegasus.Core/Operations/ServiceHealth.cs
@@ -255,7 +255,6 @@ public static class ServiceHealthPolicy
                 or ExternalWorkKinds.CreateAuditReferenceCustody
                 or ExternalWorkKinds.CreateImageCaseCustody
                 or ExternalWorkKinds.MergeImageCaseCustody => ServiceHealthDependency.Box,
-            ExternalWorkKinds.SubmitCaseToEva => ServiceHealthDependency.EvaApi,
             _ => ServiceHealthDependency.Worker
         };
 
